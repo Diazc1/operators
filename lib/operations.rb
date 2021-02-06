c99dd5bfa0db6a92a09a73f2b7f6fafaa3f746df
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-05 21:31:13 -0800

    commit tree 5d3695e02c227151dc08607ac6eaf7fec1bc54fa

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
-def unsafe?(speed)
-
+def unsafe?(speed)  #build method with if/else    should return #true if speed is below 40 or above 60 
+    if speed > 60 
+        return true
+    elsif speed < 40
+        return true
+    else
+        return false
+    end
 end
 
 
 
-def not_safe?(speed)
-	
+
+def not_safe?(speed)   #build method with previous #unsafe? method but use ?: instead of if/else
+    speed < 40 || speed > 60 ? true : false  
 end
 	
-
-
--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -1,31 +1,30 @@
-
 describe "Operations" do
   let(:file_contents) { File.read(File.join(File.dirname(__FILE__), '..', 'lib/operations.rb')) }
 
-  describe "#unsafe?" do
+  describe "#unsafe?" do 
     it 'returns true if speed is greater than 60' do
       answer = unsafe?(79)
       expect(answer).to eq(true)
     end
 
-    it 'returns true if speed is less than 40' do
+    it 'returns true if speed is less than 40' do 
       answer = unsafe?(35)
       expect(answer).to eq(true)
     end
 
-    it 'returns false if the speed is between 40 and 60' do
+    it 'returns false if the speed is between 40 and 60' do 
       answer = unsafe?(50)
       expect(answer).to eq(false)
-    end
-  end
+    end                                                   
+  end                                                     
 
-  describe "#not_safe?" do
-    it 'returns true if speed is greater than 60' do
+  describe "#not_safe?" do        
+    it 'returns true if speed is greater than 60' do    
       answer = not_safe?(79)
       expect(answer).to eq(true)
     end
 
-    it 'returns true if speed is less than 40' do
+    it 'returns true if speed is less than 40' do  
       answer = not_safe?(35)
       expect(answer).to eq(true)
     end
